3b300b660c4c706ab9d5393c1634a3e4d35cbcb4
KayodeOusi <kayodeolusi@gmail.com>
2022-05-12 12:49:58 +0100

    added methods

--- dictionaries & hash/dictionary.js
+++ dictionaries & hash/dictionary.js
@@ -12,9 +12,53 @@ export const defaultToString = (item) => {
     return item.toString();
 };
 
+class ValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    // Return an array of key-value
+    toString() {
+        return [`#${this.key}: ${this.value}`];
+    }
+}
+
 class Dictionary {
    constructor(toStrFn = defaultToString) {
        this.table = {};
        this.toStrFn = toStrFn;
    } 
+
+   // This method checks if a key exists in the dictionary
+   hasKey(key) {
+       return this.table[this.toStrFn(key)] !== null;
+   }
+
+   // This method adds a new element to the dictionary
+   set(key, value) {
+       if(key != null && value != null) {
+           // Check and convert the key to string and add to the dictionary
+           const tableKey = this.toStrFn(key);
+           this.table[tableKey] = new ValuePair(key, value);
+           return true;
+       }
+       return false;
+   }
+
+   // This method removes an element from the dictionary
+   remove(key) {
+        if(this.hasKey(key)) {
+            delete this.table[this.toStrFn(key)];
+            return true;
+        }
+        return false;
+   }
+
+
+   // This method retrieves a value from a dictionary
+   get(key) {
+       const valuePair = this.table[this.toStrFn(key)];
+       return valuePair == null ? undefined : valuePair.value;
+   }
 }
