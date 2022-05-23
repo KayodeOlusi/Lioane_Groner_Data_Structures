eeebfd9caad47ba595eee6fa2e0d3b048207b330
KayodeOusi <kayodeolusi@gmail.com>
2022-05-23 09:18:22 +0100

    recursion examples

--- Recursion/Factorials.js
+++ Recursion/Factorials.js
@@ -23,6 +23,43 @@ function factorial(n) {
     return n * factorial(n - 1);
 }
 
+// Iterative Fibonacci sequence
+function fibonacci(n) {
+    if (n < 1) return 0;
+    if (n <= 2) return 1;
+
+    let finMinus2 = 0;
+    let fibMinus1 = 1;
+    let fibN = n;
+
+    for (let i = 2; i <= n; i++) { // n >= 2
+        fibN = fibMinus1 + finMinus2; // f(n - 1) + f(n - 2)
+        fibMinus2 = fibMinus1;
+        fibMinus1 = fibN;
+    }
+
+    return fibN;
+}
+
+// Recursive Fibonacci sequence
+function recFibonacci(n) {
+    if (n < 1) return 0;
+    if (n <= 2) return 1;
+
+    return recFibonacci(n - 1) + recFibonacci(n - 2);
+}
+
+// Memoization Fibonacci
+function memoFibonacci(n) {
+    const memo = [0, 1];
+    const fibonacci = (n) => {
+        if (memo[n] != null) return memo[n];
+        return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    }
+
+    return fibonacci;
+}
+
 
 console.log(factorial(8))
 console.log(factorialIterative(8))
