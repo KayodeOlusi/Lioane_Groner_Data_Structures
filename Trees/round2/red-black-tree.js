c39bb6deff2d20e354125bd128379dc710463d62
KayodeOlusi <kayodeolusi@gmail.com>
2024-05-12 19:28:11 +0100

    inserting nodes to red black tree

--- Trees/round2/red-black-tree.js
+++ Trees/round2/red-black-tree.js
@@ -3,7 +3,7 @@ const { defaultCompare, Colors, Compare } = require("../../utils");
 const BinarySearchTree = require('./trees');
 
 class RedBlackTree extends BinarySearchTree {
-  constructor(compareFn = defaultCompare()) {
+  constructor(compareFn = defaultCompare) {
     super(compareFn);
     this.compareFn = compareFn;
     this.root = null;
@@ -35,6 +35,38 @@ class RedBlackTree extends BinarySearchTree {
     } else {
       return this.insertNode(node.right, key);
     }
+  }
+
+  fixTreeProperties(node) {
+    while (node && node.parent && node.parent.color.isRed() && node.color !== Colors.BLACK) {
+      let parent = node.parent;
+      const grandParent = parent.parent;
+
+      // Case A: If parent is left child;
+      if (grandParent && grandParent.left === parent) {
+        const uncle = grandParent.right;
+
+        // Case 1A: Uncle of node is also red - only recoloring
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else { /* empty */ }
+      } else {
+        // Case B: Parent is a right child
+        const uncle = grandParent.left;
+
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else { /* empty */ }
+      }
+    }
+
+    this.root.color = Colors.BLACK;
   }
 }
 
