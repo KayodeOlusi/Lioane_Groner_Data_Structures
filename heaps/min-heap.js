24e9e7b85bb50cc88754d27a60dad1ae3efe44cd
KayodeOlusi <kayodeolusi@gmail.com>
2024-05-18 15:11:45 +0100

    heap index

---
+++ heaps/min-heap.js
@@ -0,0 +1,21 @@
+const { defaultCompare } = require("../utils");
+
+class MinHeap {
+  constructor(compareFn = defaultCompare) {
+    this.compareFn = compareFn;
+    this.heap = [];
+  }
+
+  getLeftIndex(index) {
+    return 2 * index + 1;
+  }
+
+  getRightIndex(index) {
+    return 2 * index + 2;
+  }
+
+  getParentIndex(index) {
+    if (index === 0) return undefined;
+    return Math.floor((index - 1) / 2);
+  }
+}
