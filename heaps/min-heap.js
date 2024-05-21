fa04a0c2ad5d4b3a62e439a0432be53c4f6dc782
KayodeOlusi <kayodeolusi@gmail.com>
2024-05-21 01:52:51 +0100

    inserting to heaps

--- heaps/min-heap.js
+++ heaps/min-heap.js
@@ -1,4 +1,4 @@
-const { defaultCompare } = require("../utils");
+const { defaultCompare, Compare, swap } = require("../utils");
 
 class MinHeap {
   constructor(compareFn = defaultCompare) {
@@ -18,4 +18,53 @@ class MinHeap {
     if (index === 0) return undefined;
     return Math.floor((index - 1) / 2);
   }
+
+  insert(value) {
+    if (value != null) {
+      this.heap.push(value);
+      this.siftUp(this.heap.length - 1);
+      return true;
+    }
+    return false;
+  }
+
+  siftUp(index) {
+    let parent = this.getParentIndex(index);
+
+    console.log(index, parent);
+    while (index > 0 && this.compareFn(this.heap[parent], this.heap[index]) === Compare.BIGGER_THAN) {
+      swap(this.heap, parent, index);
+      index = parent;
+      parent = this.getParentIndex(index);
+
+      console.log(this.heap);
+      console.log(index, parent, this.heap[index], this.heap[parent]);
+    }
+    console.log("-------------------");
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  findMinimum() {
+    return this.isEmpty() ? undefined : this.heap[0];
+  }
 }
+
+const heap = new MinHeap();
+heap.insert(2);
+heap.insert(-1);
+heap.insert(-5);
+heap.insert(6);
+heap.insert(1);
+heap.insert(5);
+heap.insert(4);
+heap.insert(-50);
+
+console.log(heap.findMinimum());
+
--- utils/index.js
+++ utils/index.js
@@ -22,8 +22,14 @@ const BalanceFactor = {
 };
 
 function defaultCompare(a, b) {
-  if (a === b) return 0;
+  if (a === b) return Compare.EQUALS;
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function swap(array, a, b) {
+  const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
 }
 
 module.exports = {
@@ -31,5 +37,6 @@ module.exports = {
   defaultCompare,
   BalanceFactor,
   Compare,
-  Colors
+  Colors,
+  swap
 };
