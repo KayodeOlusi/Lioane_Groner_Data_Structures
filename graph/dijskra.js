0ea1d5697bc5d94be68a27bff6e734b4d9efb45e
KayodeOlusi <kayodeolusi@gmail.com>
2024-08-27 09:16:48 +0100

    dijskra algorithm

--- graph/dijskra.js
+++ graph/dijskra.js
@@ -7,30 +7,6 @@ const graph = [
   [0, 0, 0, 0, 0, 0]
 ];
 const INF = Number.MAX_SAFE_INTEGER;
-const dijskra = (graph, src) => {
-  const dist = [];
-  const visited = [];
-
-  const { length } = graph;
-  for (let i = 0; i < length; i++) {
-    dist[i] = INF;
-    visited[i] = false;
-  }
-
-  dist[src] = 0;
-  for (let i = 0; i < length - 1; i++) {
-    const u = minDistance(dist, visited); // 3
-    visited[u] = true;
-
-    for (let v = 0; v < length; v++) {
-      if (!visited[v] && graph[u][v] !== 0 && dist[u] !== INF && dist[u] + graph[u][v] < dist[v]) {
-        dist[v] = dist[u] + graph[u][v];
-      }
-    }
-  }
-
-  return dist;
-};
 
 const minDistance = (dist, visited) => {
   let min = INF;
@@ -43,6 +19,31 @@ const minDistance = (dist, visited) => {
   }
 
   return minIndex;
+};
+
+const dijskra = (graph, src) => {
+  const dist = [];
+  const visited = [];
+
+  const { length } = graph;
+  for (let i = 0; i < length; i++) {
+    dist[i] = INF;
+    visited[i] = false;
+  }
+
+  dist[src] = 0;
+  for (let i = 0; i < length - 1; i++) {
+    const u = minDistance(dist, visited);
+    visited[u] = true;
+
+    for (let v = 0; v < length; v++) {
+      if (!visited[v] && graph[u][v] !== 0 && dist[u] !== INF && dist[u] + graph[u][v] < dist[v]) {
+        dist[v] = dist[u] + graph[u][v];
+      }
+    }
+  }
+
+  return dist;
 };
 
 console.log(dijskra(graph, 0));
