cb12b3a3e17ed0a0d2750e0b2b4d94839a5afb8b
KayodeOlusi <kayodeolusi@gmail.com>
2024-08-28 22:27:14 +0100

    kruskal algorithm

--- graph/prim.js
+++ graph/prim.js
@@ -1,11 +1,11 @@
 const INF = Number.MAX_SAFE_INTEGER;
 const graph = [
   [0, 2, 4, 0, 0, 0],
-  [0, 0, 1, 4, 2, 0],
-  [0, 0, 0, 0, 3, 0],
-  [0, 0, 0, 0, 0, 2],
-  [0, 0, 0, 3, 0, 2],
-  [0, 0, 0, 0, 0, 0]
+  [2, 0, 2, 4, 2, 0],
+  [4, 2, 0, 0, 3, 0],
+  [0, 4, 0, 0, 3, 2],
+  [0, 2, 3, 3, 0, 2],
+  [0, 0, 0, 2, 2, 0]
 ];
 
 const minKey = (graph, key, visited) => {
@@ -40,7 +40,7 @@ const prim = graph => {
     for (let v = 0; v < length; v++) {
       if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) {
         parent[v] = u;
-        key[u] = graph[u][v];
+        key[v] = graph[u][v];
       }
     }
   }
