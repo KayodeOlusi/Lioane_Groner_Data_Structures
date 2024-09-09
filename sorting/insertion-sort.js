d27502f4269c023911c0d273477ae57d70985c56
KayodeOlusi <kayodeolusi@gmail.com>
2024-09-10 00:36:47 +0100

    merge & insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,18 @@
+const { defaultCompare, Compare } = require("../utils");
+
+function insertionSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+  let temp;
+  for (let i = 1; i < length; i++) {
+    let j = i;
+    temp = array[i];
+    while (j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) {
+      array[j] = array[j - 1];
+      j--;
+    }
+    array[j] = temp;
+  }
+  return array;
+}
+
+console.log(insertionSort([3, 5, 1, 4, 2]));
---
+++ sorting/merge-sort.js
@@ -0,0 +1,25 @@
+const { defaultCompare, Compare } = require("../utils");
+
+function mergeSort(array, compareFn = defaultCompare) {
+  if (array.length > 1) {
+    const { length } = array;
+    const middle = Math.floor(length / 2);
+    const left = mergeSort(array.slice(0, middle), compareFn);
+    const right = mergeSort(array.slice(middle, length), compareFn);
+    array = merge(left, right, compareFn);
+  }
+
+  return array;
+}
+
+function merge(left, right, compareFn = defaultCompare) {
+  let i = 0;
+  let j = 0;
+  const result = [];
+  while (i < left.length && j < right.length) {
+    result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++]);
+  }
+  return result.concat(i < left.length ? left.slice(i) : right.slice(j));
+}
+
+console.log(mergeSort([3, 5, 1, 4, 2]));
